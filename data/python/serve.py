83db0f9038141b59ca2a43496ea7d1a35f81d245
Elliot <apachehaisley@gmail.com>
2025-11-30 13:39:44 -0500

    move serve script to subdir

--- data/python/serve.py
+++ data/python/serve.py
@@ -1,11 +1,12 @@
 import http.server
+import sys
 
 HOST = '0.0.0.0'
 PORT = 8080
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory='install/share', **kwargs)
+        super().__init__(*args, directory=sys.argv[1], **kwargs)
 
     def end_headers(self):
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
@@ -14,4 +15,5 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
 
 with http.server.HTTPServer((HOST, PORT), MyHandler) as httpd:
     print(f'Serving on http://{HOST}:{PORT}')
+    print(f'Serving from directory: {sys.argv[1]}')
     httpd.serve_forever()
